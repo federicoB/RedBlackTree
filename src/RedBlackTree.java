ff424d5320e4abff7eed2e24586274e35b363755
federicoB <federicobertani96@gmail.com>
2016-03-13 16:58:13 +0100

    Added class generics and the two children.

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,5 +1,10 @@
 /**
  * Created by Federico Bertani on 13/03/16.
  */
-public class RedBlackTree {
+public class RedBlackTree<ItemType extends Comparable> {
+
+    private RedBlackTree<ItemType> leftChild;
+
+    private RedBlackTree<ItemType> rightChild;
+
 }
