7fa95a3f16cf1104326fa6edeec384e570f160d0
federicoB <federicobertani96@gmail.com>
2016-03-14 09:22:14 +0100

    Added enum for color and color field. Added documentation.

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,14 +1,35 @@
 /**
  * Created by Federico Bertani on 13/03/16.
  */
+
+/**
+ * RedBlackTree is a type of Data structure. It's a binary search tree with auto-balance.
+ *
+ * @param <ItemType> the type of data that the tree will contain.
+ */
 public class RedBlackTree<ItemType extends Comparable> {
 
+    /**
+     * The left subtree.
+     */
     private RedBlackTree<ItemType> leftChild;
-
+    /**
+     * The right subtree.
+     */
     private RedBlackTree<ItemType> rightChild;
-
+    /**
+     * The value of the node.
+     */
     private ItemType value;
+    /**
+     * The color of the node.
+     */
+    private RBColor color;
 
+    /**
+     * Create a Tree with a given value.
+     * @param value ItemType: the value of the node to create.
+     */
     public RedBlackTree(ItemType value) {
         this.value = value;
         this.leftChild = null;
@@ -19,5 +40,14 @@ public class RedBlackTree<ItemType extends Comparable> {
 
         return null;
     }
+
+    /**
+     * Define an enum for the color of the node.
+     */
+    private enum RBColor {
+        BLACK,RED
+    }
+
+
 
 }
