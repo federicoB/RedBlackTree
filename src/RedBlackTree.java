90d1f250cc6f4285ed047c1b85d34d6498706402
federicoB <federicobertani96@gmail.com>
2016-03-29 17:41:33 +0200

    Implemented lookup and fixed constructor.

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -5,9 +5,10 @@
 /**
  * RedBlackTree is a type of Data structure. It's a binary search tree with auto-balance.
  *
- * @param <ItemType> the type of data that the tree will contain.
+ * @param <ItemType extends Comparable<ItemType>> the type of data that the tree will contain.
+ *                  It must implement the comparable interface and being comparable with his own.
  */
-public class RedBlackTree<ItemType extends Comparable> {
+public class RedBlackTree<ItemType extends Comparable<ItemType>> {
 
     /**
      * The left subtree.
@@ -27,11 +28,13 @@ public class RedBlackTree<ItemType extends Comparable> {
     private RBColor color;
 
     /**
-     * Create a Tree with a given value.
+     * Create a new Tree with a given value.
+     *
      * @param value ItemType: the value of the node to create.
      */
     public RedBlackTree(ItemType value) {
         this.value = value;
+        this.color = RBColor.BLACK;
         this.leftChild = null;
         this.rightChild = null;
     }
@@ -43,12 +46,23 @@ public class RedBlackTree<ItemType extends Comparable> {
      * @return RedBlackTree&lt;ItemType&gt; : return the searched node if found, null otherwise.
      */
     public RedBlackTree<ItemType> lookUpNode(ItemType item) {
-
-        return null;
+        if (this.value == null) return null;
+        else {
+            int comparison = this.value.compareTo(item);
+            if (comparison == 0) return this;
+            else if (comparison < 0) {
+                if (this.leftChild == null) return null;
+                else return this.leftChild.lookUpNode(item);
+            } else {
+                if (this.rightChild == null) return null;
+                else return this.rightChild.lookUpNode(item);
+            }
+        }
     }
 
     /**
      * Get the next nearest node.
+     *
      * @return RedBlackTree&lt;ItemType&gt; : the successor node.
      */
     private RedBlackTree<ItemType> successorNode() {
@@ -58,6 +72,7 @@ public class RedBlackTree<ItemType extends Comparable> {
 
     /**
      * Get the previous nearest node.
+     *
      * @return RedBlackTree&lt;ItemType&gt; : the predecessor node.
      */
     private RedBlackTree<ItemType> predecessorNode() {
@@ -67,13 +82,14 @@ public class RedBlackTree<ItemType extends Comparable> {
 
     /**
      * Get the minimum item of the tree.
+     *
      * @return ItemType: the minimum item of the tree.
      */
     public ItemType min() {
 
         return null;
     }
-    
+
     public ItemType max() {
 
         return null;
@@ -83,7 +99,7 @@ public class RedBlackTree<ItemType extends Comparable> {
      * Define an enum for the color of the node.
      */
     private enum RBColor {
-        BLACK,RED
+        BLACK, RED
     }
 
 
